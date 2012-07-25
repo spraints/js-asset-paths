5564fa1ac9b6912fb0566f0924ceef759feb21cc
Matt Burke <spraints@gmail.com>
2012-07-25 14:51:22 -0400

    new gem

---
+++ lib/js-asset-paths.rb
@@ -0,0 +1,9 @@
+require "js-asset-paths/version"
+
+module Js
+  module Asset
+    module Paths
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/js-asset-paths/version.rb
@@ -0,0 +1,7 @@
+module Js
+  module Asset
+    module Paths
+      VERSION = "0.0.1"
+    end
+  end
+end
